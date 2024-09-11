37becb9ae3a60bee9d21f05edf9b0760056958e7
Liam Ozog <liam.ozog@gmail.com>
2024-09-11 13:54:06 -0400

    add prettier

--- src/actors/tile/tile.ts
+++ src/actors/tile/tile.ts
@@ -1,49 +1,54 @@
 import { Actor, Color, Input, Polygon, PolygonCollider, vec } from "excalibur";
 
 const SIDE_LENGTH = 23; // TODO: needs to stay in sync with circumradius
-const HEX_WIDTH = 1.73 * SIDE_LENGTH // sqrt(3) * SIDE_LENGTH
+const HEX_WIDTH = 1.73 * SIDE_LENGTH; // sqrt(3) * SIDE_LENGTH
 const RESOURCE_COLORS = {
-  'wood': Color.Green,
-  'sheep': Color.Orange,
-  'wheat': Color.Yellow,
-  'ore': Color.Violet,
-  'brick': Color.Red,
-  'desert': Color.LightGray,
-  'sea': Color.ExcaliburBlue
-}
+    wood: Color.Green,
+    sheep: Color.Orange,
+    wheat: Color.Yellow,
+    ore: Color.Violet,
+    brick: Color.Red,
+    desert: Color.LightGray,
+    sea: Color.ExcaliburBlue,
+};
 const HEX_POINTS = [
-  vec(-1, Math.sqrt(3)).scale(SIDE_LENGTH),
-  vec(1, Math.sqrt(3)).scale(SIDE_LENGTH),
-  vec(2, 0).scale(SIDE_LENGTH),
-  vec(1, -1 * Math.sqrt(3)).scale(SIDE_LENGTH),
-  vec(-1, -1 * Math.sqrt(3)).scale(SIDE_LENGTH),
-  vec(-2, 0).scale(SIDE_LENGTH),
+    vec(-1, Math.sqrt(3)).scale(SIDE_LENGTH),
+    vec(1, Math.sqrt(3)).scale(SIDE_LENGTH),
+    vec(2, 0).scale(SIDE_LENGTH),
+    vec(1, -1 * Math.sqrt(3)).scale(SIDE_LENGTH),
+    vec(-1, -1 * Math.sqrt(3)).scale(SIDE_LENGTH),
+    vec(-2, 0).scale(SIDE_LENGTH),
 ];
 
 export class Tile extends Actor {
-  private resource: string;
+    private resource: string;
 
-  constructor(x: number, y: number, resource: string) {
-    // need to set a collider with the same shape as the graphics since useGraphicsBounds doesn't seem to work
-    const hexagonCollider = new PolygonCollider({
-      points: HEX_POINTS
-    });
-    super({
-      x, y, color: RESOURCE_COLORS[resource], collider: hexagonCollider
-    });
-    this.resource = resource
-    this.pointer.useGraphicsBounds = false
-  }
+    constructor(x: number, y: number, resource: string) {
+        // need to set a collider with the same shape as the graphics since useGraphicsBounds doesn't seem to work
+        const hexagonCollider = new PolygonCollider({
+            points: HEX_POINTS,
+        });
+        super({
+            x,
+            y,
+            color: RESOURCE_COLORS[resource],
+            collider: hexagonCollider,
+        });
+        this.resource = resource;
+        this.pointer.useGraphicsBounds = false;
+    }
 
-  public onInitialize() {
-    const hexagon = new Polygon({
-      points: HEX_POINTS,
-      color: this.color
-    });
-    this.graphics.use(hexagon);
+    public onInitialize() {
+        const hexagon = new Polygon({
+            points: HEX_POINTS,
+            color: this.color,
+        });
+        this.graphics.use(hexagon);
 
-    this.on('pointerdown', (evt: Input.PointerEvent) => {
-      console.log(`${this.resource} tile at ${this.pos.x}, ${this.pos.y} was clicked`)
-    })
-  }
+        this.on("pointerdown", (evt: Input.PointerEvent) => {
+            console.log(
+                `${this.resource} tile at ${this.pos.x}, ${this.pos.y} was clicked`
+            );
+        });
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
-import { Engine, Loader, DisplayMode, CollisionType, Color, Input } from 'excalibur';
-import { Tile } from './actors/tile/tile';
-import { ScenarioBuilder } from './lib/scenariobuilder';
+import {
+    Engine,
+    Loader,
+    DisplayMode,
+    CollisionType,
+    Color,
+    Input,
+} from "excalibur";
+import { Tile } from "./actors/tile/tile";
+import { ScenarioBuilder } from "./lib/scenariobuilder";
 // import { Resources } from './resources';
 
 /**
  * Managed game class
  */
 class Game extends Engine {
-
-  constructor() {
-    super({ displayMode: DisplayMode.FitScreen });
-  }
+    constructor() {
+        super({ displayMode: DisplayMode.FitScreen });
+    }
 }
 
 const game = new Game();
 // const loader = new Loader(Object.values(Resources));
 // game.start(loader);
-game.start()
+game.start();
 
 // // this represents the hex board. Each row contains a 1 for a drawn hex or a 0 for a "ghost" hex
 // // (used for positioning)
@@ -43,18 +49,18 @@ const tiles: Tile[] = [];
 //   }
 // }
 
-
-
 const scenarioBuilder = new ScenarioBuilder();
 
 const scenario = scenarioBuilder.getScenario();
 const offset = 300;
 for (const tile of scenario.board.tiles) {
-  tiles.push(new Tile(tile.center.x + offset, tile.center.y + offset, tile.type))
+    tiles.push(
+        new Tile(tile.center.x + offset, tile.center.y + offset, tile.type)
+    );
 }
-console.log(scenario.board)
+console.log(scenario.board);
 
 tiles.forEach(function (tile) {
-  // Add the tile to the current scene to be drawn
-  game.add(tile);
+    // Add the tile to the current scene to be drawn
+    game.add(tile);
 });
